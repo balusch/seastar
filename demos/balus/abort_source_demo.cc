24cb6a25f246ab49367ba1273251a89c2199061f
balus <balus@foxmail.com>
2022-03-31 19:59:51 +0800

    Chore: format code of demos.

--- demos/balus/abort_source_demo.cc
+++ demos/balus/abort_source_demo.cc
@@ -4,26 +4,23 @@
 
 #include <chrono>
 
-#include <seastar/core/future.hh>
-#include <seastar/core/sleep.hh>
-#include <seastar/core/future-util.hh>
-#include <seastar/core/abort_source.hh>
-#include <seastar/core/app-template.hh>
+#include "seastar/core/abort_source.hh"
+#include "seastar/core/app-template.hh"
+#include "seastar/core/future-util.hh"
+#include "seastar/core/future.hh"
+#include "seastar/core/sleep.hh"
 
 namespace ss = seastar;
 
 extern ss::future<> f();
 
-int
-main(int argc, char **argv)
-{
+int main(int argc, char** argv) {
     ss::app_template app;
     return app.run(argc, argv, f);
 }
 
 ss::future<> f() {
     using namespace std::chrono_literals;
-    return ss::sleep(3s).then([]() {
-        std::cout << "sleep done..." << std::endl;
-    });
+    return ss::sleep(3s).then(
+        []() { std::cout << "sleep done..." << std::endl; });
 }
--- demos/balus/semaphore_demo.cc
+++ demos/balus/semaphore_demo.cc
@@ -1,15 +1,15 @@
 
-#include <iostream>
 #include <chrono>
+#include <iostream>
 
-#include "seastar/core/reactor.hh"
-#include "seastar/core/smp.hh"
-#include "seastar/core/gate.hh"
-#include "seastar/core/thread.hh"
 #include "seastar/core/app-template.hh"
-#include "seastar/core/sleep.hh"
-#include "seastar/core/future.hh"
 #include "seastar/core/future-util.hh"
+#include "seastar/core/future.hh"
+#include "seastar/core/gate.hh"
+#include "seastar/core/reactor.hh"
+#include "seastar/core/sleep.hh"
+#include "seastar/core/smp.hh"
+#include "seastar/core/thread.hh"
 
 namespace ss = seastar;
 
@@ -27,9 +27,7 @@ static ss::future<> f2() __attribute__((unused));
 static ss::future<> f3() __attribute__((unused));
 static ss::future<> f4() __attribute__((unused));
 
-int
-main(int argc, char **argv)
-{
+int main(int argc, char** argv) {
     ss::app_template app;
     return app.run(argc, argv, f1);
 }
@@ -37,7 +35,7 @@ main(int argc, char **argv)
 // balus(n): Test evaluation order
 static ss::future<> f() {
     using namespace std::chrono_literals;
-    return ss::sleep(1s).then([c = counter(1)] () {
+    return ss::sleep(1s).then([c = counter(1)]() {
         std::cout << "first sleep done" << std::endl;
         return ss::sleep(1s).then([c = counter(2)]() {
             std::cout << "second sleep done" << std::endl;
@@ -54,13 +52,18 @@ static ss::future<> slow() {
 // balus(n): Test semaphore wait/signal
 static ss::future<> f1() {
     static ss::semaphore sem(10);
-    return sem.wait(1).then([]() {
-        std::cout << "semaphore wait OK: " << sem.available_units() << std::endl;
-        return slow().finally([]() {
-            sem.signal(1);
-            std::cout << "slow finally: semaphore signal OK: " << sem.available_units() << std::endl;
+    return sem.wait(1)
+        .then([]() {
+            std::cout << "semaphore wait OK: " << sem.available_units()
+                      << std::endl;
+            return slow().finally([]() {
+                sem.signal(1);
+                std::cout << "slow finally: semaphore signal OK: "
+                          << sem.available_units() << std::endl;
+            });
+        })
+        .finally([]() {
+            std::cout << "wait finally: semaphore signal OK: "
+                      << sem.available_units() << std::endl;
         });
-    }).finally([]() {
-        std::cout << "wait finally: semaphore signal OK: " << sem.available_units() << std::endl;
-    });
 }
