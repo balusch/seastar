973cb228769d2c8e2e22526a361bc886315fb8bf
balus <balus@foxmail.com>
2022-04-11 21:04:43 +0800

    Balus: add demo about syscall thread.

--- demos/balus/thread_pool_demo.cc
+++ demos/balus/thread_pool_demo.cc
@@ -1,4 +1,3 @@
-
 #include <memory>
 
 #include "../../src/core/thread_pool.hh"
@@ -7,32 +6,48 @@
 
 namespace ss = seastar;
 
+[[maybe_unused]] static ss::future<> f1();
+[[maybe_unused]] static ss::future<> f2();
+
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        fmt::fprintf(stderr, "usage: {} $file\n", argv[0]);
-        exit(-1);
-    }
-
     ss::app_template app;
-    app.run(argc, argv, [f = argv[1]]() {
-        // auto tp = std::make_unique<ss::thread_pool>(&ss::engine(), "laputa");
-
-        return ss::engine()
-            .file_stat(f, ss::follow_symlink::no)
-            .then([f](ss::stat_data &&sd) {
-                fmt::print(
-                    "device_id:{}\tinode_number:{}\tmode:{}\ttype:{}\tnlinks:{}"
-                    "\tuid:{}\tgid:{}\trdev:{}\tsize:{}\tblock_size:{}"
-                    "\tallocated_size:{}\tatime:{}\tmtime:{}\tctime{}\n",
-                    sd.device_id, sd.inode_number, sd.mode, sd.type,
-                    sd.number_of_links, sd.uid, sd.gid, sd.rdev, sd.size,
-                    sd.block_size, sd.allocated_size,
-                    sd.time_accessed.time_since_epoch().count(),
-                    sd.time_modified.time_since_epoch().count(),
-                    sd.time_changed.time_since_epoch().count());
-                return ss::make_ready_future<>();
-            });
-    });
+    app.run(argc, argv, []() { return f1(); });
 
     return 0;
+}
+
+static ss::future<> f1() {
+    const char *f = "hello.txt";
+    return ss::engine()
+        .file_stat(f, ss::follow_symlink::no)
+        .then([](ss::stat_data &&sd) {
+            fmt::print(
+                "device_id:{}\tinode_number:{}\tmode:{}\ttype:{}\tnlinks:{}"
+                "\tuid:{}\tgid:{}\trdev:{}\tsize:{}\tblock_size:{}"
+                "\tallocated_size:{}\tatime:{}\tmtime:{}\tctime{}\n",
+                sd.device_id, sd.inode_number, sd.mode, (int)sd.type,
+                sd.number_of_links, sd.uid, sd.gid, sd.rdev, sd.size,
+                sd.block_size, sd.allocated_size,
+                sd.time_accessed.time_since_epoch().count(),
+                sd.time_modified.time_since_epoch().count(),
+                sd.time_changed.time_since_epoch().count());
+            return ss::make_ready_future<>();
+        });
+}
+
+/* balus(N): bad example */
+static ss::future<> f2() {
+    auto tp = std::make_shared<ss::thread_pool>(&ss::engine(), "laputa");
+    ss::engine().at_exit([tp]() {
+        fmt::print("tp exit()\n");
+        return ss::make_ready_future<>();
+    });
+    return tp
+        ->submit<uint64_t>([tp]() {
+            struct stat info;
+            int rc = ::stat("hello.txt", &info);
+            ss::throw_system_error_on(rc == -1, "stat() failed");
+            return (uint64_t)info.st_size;
+        })
+        .then([](uint64_t &&size) { fmt::print("size: {}\n", size); });
 }
