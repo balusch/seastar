94a55a610543d19b4250270478dfaac03ae18454
Jianyong Chen <balus@foxmail.com>
2022-07-09 14:35:37 +0800

    Add demo about iostream.
    Signed-off-by: Jianyong Chen <balus@foxmail.com>

--- demos/balus/iostream_demo.cc
+++ demos/balus/iostream_demo.cc
@@ -1,7 +1,11 @@
 
-#include "seastar/core/app-template.hh"
-#include "seastar/core/future.hh"
-#include "seastar/core/sleep.hh"
+#include <seastar/core/app-template.hh>
+#include <seastar/core/file.hh>
+#include <seastar/core/fstream.hh>
+#include <seastar/core/future.hh>
+#include <seastar/core/iostream.hh>
+#include <seastar/core/reactor.hh>
+#include <seastar/core/sleep.hh>
 
 namespace ss = seastar;
 
@@ -13,6 +17,25 @@ int main(int argc, char **argv) {
 }
 
 ss::future<> f1() {
-    using namespace std::chrono_literals;
-    return ss::sleep(1s).then([]() { fmt::print("sleep done...\n"); });
+    static char small_buf[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    static char large_buf[] =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    return ss::open_file_dma("hello.txt", ss::open_flags::wo |
+                                              ss::open_flags::create |
+                                              ss::open_flags::exclusive)
+        .then([](ss::file &&f) {
+            return ss::make_file_output_stream(f, 32).then(
+                [](ss::output_stream<char> &&os) mutable {
+                    return os.write(small_buf, sizeof(small_buf))
+                        .then([os = std::move(os)]() mutable {
+                            return os.write(large_buf, sizeof(large_buf));
+                        });
+                });
+        });
 }
