105c78bb41b8db34bd61b25cd13ee70ecf5cca8d
Jianyong Chen <balus@foxmail.com>
2022-07-27 23:24:24 +0800

    Demo: add socket demo.
    Signed-off-by: Jianyong Chen <balus@foxmail.com>

--- demos/balus/net_socket_demo.cc
+++ demos/balus/net_socket_demo.cc
@@ -1,18 +1,26 @@
 
 #include "seastar/core/app-template.hh"
 #include "seastar/core/future.hh"
+#include "seastar/core/reactor.hh"
 #include "seastar/core/sleep.hh"
+#include "seastar/net/api.hh"
 
 namespace ss = seastar;
 
-ss::future<> f1();
+[[maybe_unused]] static ss::future<> f1();
 
 int main(int argc, char **argv) {
     ss::app_template app;
     return app.run(argc, argv, f1);
 }
 
-ss::future<> f1() {
-    using namespace std::chrono_literals;
-    return ss::sleep(1s).then([]() { fmt::print("sleep done...\n"); });
+static ss::future<> f1() {
+    ss::listen_options lopt;
+    ss::socket_address sa(uint16_t(9877));
+    ss::server_socket s = ss::engine().listen(sa, lopt);
+    return s.accept().then([](ss::accept_result ar) {
+        auto conn = std::move(ar.connection);
+        auto peer = std::move(ar.remote_address);
+        return ss::make_ready_future<>();
+    });
 }
